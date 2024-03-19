abb39baaf539081008ee5df2f258b55f8e9f0dfb
Joel Yong <yongwenzhuo@gmail.com>
2024-03-19 20:17:09 +0800

    fix hook

--- pkg/hook/emitter.go
+++ pkg/hook/emitter.go
@@ -54,21 +54,32 @@ func (emitter *Emitter[T]) Emit(data T) {
 	emitter.mu.RLock()
 	defer emitter.mu.RUnlock()
 
+	if emitter.debounceTimer != nil {
+		emitter.debounceTimer.Stop()
+	}
+
 	for _, ch := range emitter.listeners {
 		ch <- data
 	}
 }
 
 func (emitter *Emitter[T]) EmitWithDebounce(data T, duration time.Duration) {
-	if emitter.debounceTimer == nil {
-		emitter.debounceTimer = time.NewTimer(duration)
-	} else {
-		emitter.debounceTimer.Reset(duration)
+	if emitter.debounceTimer != nil {
+		emitter.debounceTimer.Stop()
 	}
 
-	go func(timer *time.Timer) {
+	timer := time.NewTimer(duration)
+	emitter.debounceTimer = timer
+
+	id := gonanoid.Must(4)
+
+	// fmt.Printf("id: %s, data %+v\n", id, data)
+
+	go func(timer *time.Timer, data T, id string) {
 		<-timer.C
+		// fmt.Printf("%s: emitting data %+v\n", id, data)
+
 		emitter.Emit(data)
-	}(emitter.debounceTimer)
+	}(timer, data, id)
 
 }
